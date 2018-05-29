bce8c5b0b923839ee7f881b6efed836315ff8e7f
Paschall Davis <paschall913@gmail.com>
2018-05-28 20:30:52 -0400

    first commit; hibernate throwing type mismatch error

---
+++ src/main/java/tracker/TaskEntity.java
@@ -0,0 +1,35 @@
+package tracker;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.*;
+
+
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "Tasks")
+public class TaskEntity {
+
+    @Id
+    //@GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    @NonNull
+    private String name;
+    @NonNull
+    private Integer period;
+    @NonNull
+    private Integer weight;
+
+    public TaskEntity(String name, Integer period, Integer weight) {
+        this.name = name;
+        this.period = period;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
---
+++ src/main/java/tracker/TaskRepository.java
@@ -0,0 +1,11 @@
+package tracker;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
+import javax.transaction.Transactional;
+
+@Transactional
+public interface TaskRepository extends CrudRepository<TaskEntity, Integer> {
+
+}
