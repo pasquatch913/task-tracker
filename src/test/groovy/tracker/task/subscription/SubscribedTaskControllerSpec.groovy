9b284995d70005ae590e9d658280c8167fff0f49
Paschall Davis <paschall913@gmail.com>
2019-09-06 05:59:35 -0400

    controller tests for updates to task subscription

--- src/test/groovy/tracker/task/subscription/SubscribedTaskControllerSpec.groovy
+++ src/test/groovy/tracker/task/subscription/SubscribedTaskControllerSpec.groovy
@@ -8,6 +8,7 @@ import spock.lang.Subject
 import tracker.task.DataFixture
 import tracker.user.UserService
 
+import static java.lang.Boolean.TRUE
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
@@ -41,6 +42,40 @@ class SubscribedTaskControllerSpec extends Specification {
 
         then:
         model.get("periods").size() == Arrays.asList(TaskPeriod.values()).size()
+    }
+
+    def "requests for the update subscription details page return the update view"() {
+        given:
+        def taskId = 3
+        when:
+        def response = mockMvc.perform(get("${baseURL}updateTask/${taskId}"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("updateTaskSubscriptionView"))
+                .andExpect(model().attributeExists("periods"))
+                .andReturn()
+        def model = response.modelAndView.model
+
+        then:
+        1 * mockSubService.getTaskSubscriptionById(_) >> new TaskSubscriptionDTO(id: taskId)
+        model.get("periods").size() == 3
+        model.get("subscription").id == taskId
+    }
+
+    def "requests to update task subscription details result in service method calls"() {
+        given:
+        def requestParams = ['id': 1, 'name': "my updated name"]
+
+        when:
+        mockMvc.perform(
+                post("${baseURL}/updateTask")
+                        .param("id", requestParams.get("id").toString())
+                        .param("name", requestParams.get("name"))
+        )
+                .andExpect(status().isFound())
+                .andReturn()
+
+        then:
+        1 * mockSubService.updateTask(new TaskSubscriptionDTO(id: requestParams.id, name: requestParams.name, active: TRUE))
     }
 
     def "requests to create new task subscription invoke service methods and redirect"() {
