54ec06d096b41f88f6a7009691f742a7bbbe1f14
Paschall Davis <paschall913@gmail.com>
2019-09-16 20:37:43 -0400

    complete task instances/subscriptions is now done via update page

--- src/main/resources/static/js/TaskSubscriptions.js
+++ src/main/resources/static/js/TaskSubscriptions.js
@@ -1,27 +1,3 @@
-function completeTaskSubscription(id) {
-    var url = '/web/subscriptions/complete/' + id;
-    $.ajax({
-        url: url,
-        type: 'POST',
-        success: function () {
-            console.log('request sent!')
-        }
-    });
-    document.getElementById('subscription' + id).style.display = "none";
-}
-
-function completeOneTime(id) {
-    var url = '/web/oneTimes/complete/' + id;
-    $.ajax({
-        url: url,
-        type: 'POST',
-        success: function () {
-            console.log('request sent!')
-        }
-    });
-    document.getElementById('oneTime' + id).style.display = "none";
-}
-
 
 $(document).ready(function () {
     var chNameCheckbox = $('#changeName'),
